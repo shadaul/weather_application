a1bac33e5d9dd05673a12b9e31a8b4f24ec7f8fc
daulet <shabakovdaulet@gmail.com>
2026-03-19 14:11:45 +0100

    error handling and adding env

--- pogoda.py
+++ pogoda.py
@@ -8,24 +8,42 @@ KEY_POGODA=os.getenv("apikey_pogoda")
 
 
 
-def getpogoda(city: str, key_pogoda: str):
+def getpogoda(city, key_pogoda):
     
-    url = f"http://api.weatherapi.com/v1/forecast.json"
-    params = {
-        "key": f"{key_pogoda}",
-        "q": city,
-        "days":3,
-        "aqi": "no",
-        "alerts": "no"
-    }
-
-    response = requests.get(url, params=params)
-    data = response.json()
-
-    if "error" in data:
-        return "error"
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={key_pogoda}&units=metric"
+    
+    response = requests.get(url)
+    
+    print(f"DEBUG: Status Code = {response.status_code}") 
+    
+    if response.status_code == 200:
+        data = response.json()
+        return data["main"]["temp"]
     else:
-        return data["current"]["temp_c"]
+        print(f"DEBUG: Server Response = {response.text}") 
+        return "error"
+
+
+
+# def getpogoda(city: str, key_pogoda: str):
+    
+#     url = f"http://api.weatherapi.com/v1/forecast.json"
+#     params = {
+#         "key": f"{key_pogoda}",
+#         "q": city,
+#         "days":3,
+#         "aqi": "no",
+#         "alerts": "no"
+#     }
+
+#     response = requests.get(url, params=params)
+#     print("status code: ", response.status.code)
+#     data = response.json()
+
+#     if "error" in data:
+#         return "error"
+#     else:
+#         return data["current"]["temp_c"]
 
 
 
